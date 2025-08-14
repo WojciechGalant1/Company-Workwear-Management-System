b4fd561d3384384e75146b652aeefcfa6b00b438
Wojciech Galant <wojtekgalant7@gmail.com>
2025-08-14 19:02:26 +0200

    fix

--- app/forms/dodaj_zamowienieF.php
+++ app/forms/dodaj_zamowienieF.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 include_once __DIR__ . '/../controllers/HistoriaZamowienC.php';
 include_once __DIR__ . '/../controllers/SzczegolyZamowieniaC.php';
 include_once __DIR__ . '/../controllers/UbranieC.php';
